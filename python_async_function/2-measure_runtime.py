4e01238ae200dc4f5f290b15c4b15794ad45fa2b
mbabaziemelyne <uwimbeme@gmail.com>
2024-01-10 19:38:31 +0200

    runtime

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,13 @@
+#!/usr/binenv python3
+"""measuring the runtime"""
+import time
+import asyncio
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+def measure_time(n: int, max_delay: int) -> float:
+    """returns the total time / n"""
+    start = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end = time.time()
+    total_time = end - start
+    return total_time / n
