db1dff8f49df2176fcbb930035b5a02b46a39e6c
mbabaziemelyne <uwimbeme@gmail.com>
2024-01-10 15:09:33 +0200

    multiplier

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -2,6 +2,7 @@
 """ multiplying floats"""
 from typing import Callable
 
+
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """returns a function that multiplies a float by multiplier"""
     def function(n: float):
