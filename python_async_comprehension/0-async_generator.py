9ef356407f2cf7a7375b95ae8b24e12546c32f0d
mbabaziemelyne <uwimbeme@gmail.com>
2024-01-11 16:41:53 +0200

    generator

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env pyrthon3
+"""async generator"""
+from typing import Generator
+import random
+import asyncio
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """generates a random nbr btn 0-10"""
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
