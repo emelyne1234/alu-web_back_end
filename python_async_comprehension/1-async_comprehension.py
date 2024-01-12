0f3fc7db7c87e8faa88881f7a8b58f2a002e2d81
mbabaziemelyne <uwimbeme@gmail.com>
2024-01-12 23:16:35 +0200

    comprehensions

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -9,4 +9,3 @@ async_generator = __import__('0-async_generator').async_generator
 async def async_comprehension() -> list[float]:
     """ returns 10 random numbers """
     return [y async for y in async_generator()]
-    
