b34fa80d3aeaa4b7de4f27f25b8392bfe4c4beae
mbabaziemelyne <uwimbeme@gmail.com>
2024-01-13 00:08:45 +0200

    async

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -2,11 +2,16 @@
 """async comprehensions"""
 
 import asyncio
-
 from typing import List
+
+
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> list[float]:
+async def async_comprehension() -> List[float]:
     """ returns 10 random numbers """
     return [y async for y in async_generator()]
+
+
+if __name__ == "__main__":
+    asyncio.run(async_comprehension())
