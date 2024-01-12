2578f98f335124e8d5cdf16fcab58b7e7bef684a
mbabaziemelyne <uwimbeme@gmail.com>
2024-01-12 10:26:57 +0200

    async_comprehensions

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""async comprehensions"""
+
+import asyncio
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> list[float]:
+    """returns 10 random numbers"""
+    list = [i async for i in async_generator]
+    
---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""runtime for four parallel comprehensions"""
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """ returns the measured total runtime """
+    tasks = []
+    start_time = time.time()
+    for i in range(4):
+        tasks.append(asyncio.create_task(async_comprehension()))
+    await asyncio.gather(*tasks)
+    end_time = time.time()
+    return end_time - start_time
