dd6c4da81900a901a7399c0872810dcf835c73d1
mbabaziemelyne <uwimbeme@gmail.com>
2024-01-19 08:49:45 +0200

    simple pagination

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -39,6 +39,6 @@ class Server:
         
     def index_range(self, page: int, page_size: int) -> Tuple[int, int]:
         """returns a tuple of size two containing a start index and end index"""
-        start_index = page * (page_size - page_size)
+        start_index = page * page_size - page_size
         end_index = start_index + page_size
         return (start_index, end_index)
