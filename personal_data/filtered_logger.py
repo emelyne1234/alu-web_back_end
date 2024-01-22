df18618644b2208ad26d2bf02b6c9e5e66d91a71
mbabaziemelyne <uwimbeme@gmail.com>
2024-01-22 21:52:39 +0200

    regex

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,7 +4,7 @@ from typing import List
 import re
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, 
+def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """returns the log message obfuscated"""
     for field in fields:
