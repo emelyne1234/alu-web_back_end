9cd1a30ad0d137ed0c484b1b7f535f6cd23a88a4
UWIMBABAZI Emelyne <105202770+emelyne1234@users.noreply.github.com>
2024-03-14 23:35:40 +0200

    Update 9-insert_school.py

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -5,5 +5,5 @@ from pymongo import MongoClient
 
 def insert_school(mongo_collection, **kwargs):
     """inserts a new document in the collection"""
-    new_document = mongo_collection.insert_one(kwargswargs)
+    new_document = mongo_collection.insert_one(kwargs)
     new_document.inserted_id
