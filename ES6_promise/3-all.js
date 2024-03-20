aa6ec241a5213ff420c52967590c1b48b7894db1
UWIMBABAZI Emelyne <105202770+emelyne1234@users.noreply.github.com>
2024-03-20 23:15:01 +0200

    Update 3-all.js

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,4 +1,4 @@
-import { uploadPhoto, createUser } from "./utils";
+import { uploadPhoto, createUser } from './utils;
 
 function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
@@ -8,7 +8,7 @@ function handleProfileSignup() {
       const { lastName } = values[1];
       console.log(`${body} ${firstName} ${lastName}`);
     })
-    .catch(() => console.log(Signup system offline'));
+    .catch(() => console.log('Signup system offline'));
 }
 
 export default handleProfileSignup;
