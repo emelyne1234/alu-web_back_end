6f51fc822ebc5ae602bafb8d89bed21b637ad992
UWIMBABAZI Emelyne <105202770+emelyne1234@users.noreply.github.com>
2024-03-20 22:54:21 +0200

    Update 2-then.js

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -2,10 +2,10 @@ function handleResponseFromAPI(promise) {
   return promise
     .then(() => ({
       status: 200,
-      body: "success",
+      body: 'success',
     }))
     .catch(() => Error())
-    .finally(() => console.warn("Got a response from the API"));
+    .finally(() => console.warn('Got a response from the API'));
 }
 
 export default handleResponseFromAPI;
