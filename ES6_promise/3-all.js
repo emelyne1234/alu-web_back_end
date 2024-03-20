df8be071733db8588ffd0e7d32dd6f2d444182b3
UWIMBABAZI Emelyne <105202770+emelyne1234@users.noreply.github.com>
2024-03-20 23:17:06 +0200

    Update 3-all.js

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,4 +1,4 @@
-import { uploadPhoto, createUser } from './utils;
+import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
