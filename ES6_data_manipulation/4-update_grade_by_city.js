273cd70c6b0185d3dde0f66064a1bae1859d4549
UWIMBABAZI Emeline <105202770+emelyne1234@users.noreply.github.com>
2024-03-30 23:43:21 +0200

    Update 4-update_grade_by_city.js

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,14 +1,13 @@
-const updateStudentGradeByCity = (students, city, newGrade) =>
-  students
+const updateStudentGradeByCity = (students, city, newGrade) => students
     .filter((student) => student.location === city)
     .map((item) => {
       const newRecord = { ...item };
 
       const newStudent = newGrade.find(
-        (student) => student.studentId === item.id
+        (student) => student.studentId === item.id,
       );
       if (newStudent) newRecord.grade = newStudent.grade;
-      else newRecord.grade = "N/A";
+      else newRecord.grade = 'N/A';
       return newRecord;
     });
 
