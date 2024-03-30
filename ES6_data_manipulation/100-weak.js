539b049f12d699d43d5edd056468c61ffe074eec
UWIMBABAZI Emeline <105202770+emelyne1234@users.noreply.github.com>
2024-03-30 07:51:09 +0200

    Update 100-weak.js

--- ES6_data_manipulation/100-weak.js
+++ ES6_data_manipulation/100-weak.js
@@ -3,7 +3,7 @@ const weakMap = new WeakMap();
 const queryAPI = (endpoint) => {
   let total = weakMap.get(endpoint) || 0;
   weakMap.set(endpoint, (total -= -1));
-  if (total >= 5) throw new Error("Endpoint load is high");
+  if (total >= 5) throw new Error('Endpoint load is high');
   return total;
 };
 
