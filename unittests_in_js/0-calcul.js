b5cbfcf5cedb7b7da7b8dbec475f2293af6f32ed
Emeline Uwimbabazi <105202770+emelyne1234@users.noreply.github.com>
2024-04-06 18:26:33 +0200

    testing js

--- unittests_in_js/0-calcul.js
+++ unittests_in_js/0-calcul.js
@@ -3,7 +3,7 @@ module.exports = function calculateNumber(a, b = 0) {
   const bNum = Number(b);
 
   if (Number.isNaN(aNum) || Number.isNaN(bNum))
-    throw TypeError("Parameters must be numbers");
+    throw TypeError('Parameters must be numbers');
 
   return Math.round(aNum) + Math.round(bNum);
 };
