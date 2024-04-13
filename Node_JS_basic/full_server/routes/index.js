3a24f1f5c7e9f1c308d3fb3c982ed30365e1a82f
Emeline Uwimbabazi <105202770+emelyne1234@users.noreply.github.com>
2024-04-13 22:43:41 +0200

    Update index.js

--- Node_JS_basic/full_server/routes/index.js
+++ Node_JS_basic/full_server/routes/index.js
@@ -1,20 +1,20 @@
-import express from "express";
-import AppController from "../controllers/AppController";
-import StudentsController from "../controllers/StudentsController";
+import express from 'express';
+import AppController from '../controllers/AppController';
+import StudentsController from '../controllers/StudentsController';
 
 function controllerRouting(app) {
   const router = express.Router();
-  app.use("/", router);
+  app.use('/', router);
 
-  router.get("/", (req, res) => {
+  router.get('/', (req, res) => {
     AppController.getHomepage(req, res);
   });
 
-  router.get("/students", (req, res) => {
+  router.get('/students', (req, res) => {
     StudentsController.getAllStudents(req, res, process.argv[2]);
   });
 
-  router.get("/students/:major", (req, res) => {
+  router.get('/students/:major', (req, res) => {
     StudentsController.getAllStudentsByMajor(req, res, process.argv[2]);
   });
 }
