1e39a4d0b013a7b93711f6630649666bcf4c3318
Emeline Uwimbabazi <105202770+emelyne1234@users.noreply.github.com>
2024-04-13 22:32:42 +0200

    Update 4-http.js

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,11 +1,11 @@
-const http = require("http");
+const http = require('http');
 
-const hostname = "127.0.0.1";
+const hostname = '127.0.0.1';
 const port = 1245;
 
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
+  res.setHeader('Content-Type', 'text/plain');
   res.end("Hello Holberton School!");
 });
 
